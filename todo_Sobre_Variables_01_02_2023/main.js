cb4a250054d816e330b9bbd5cd2c2d39022b8a41
ShirleyDanielaPerezMancilla <shirleydp1109@gmail.com>
2023-02-01 20:36:44 -0500

    todo_Sobre_Variables

---
+++ todo_Sobre_Variables_01_02_2023/main.js
@@ -0,0 +1,6 @@
+let mensaje = "¡Hola, soy Shirley y este es mi primer taller!";
+let ejercicio = 4;
+let es_dificil = false;
+let mensajeFinal = mensaje+ " Este es el ejercicio "+ejercicio+". ¿Está muy difícil? "+es_dificil;
+
+alert(mensajeFinal);
